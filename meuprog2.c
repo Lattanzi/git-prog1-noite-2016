18e481554397520160dc9015a53f0857571f853e
lucas lattanzi <lucas_latta@hotmail.com>
2016-04-19 00:56:08 +0000

    meu primeiro commit no github

---
+++ meuprog2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nBoa noite\n");
+    printf("\nMeu nome eh Lucas\n");
+    printf("\nEste eh meu segundo programa em C\n");
+    
+    return 0;
+}
